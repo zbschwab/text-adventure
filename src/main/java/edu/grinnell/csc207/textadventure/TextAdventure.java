79b6298a77a8a90c469f1a0282ed2879d69b310c
Z Schwab <zbajraktarischwab@gmail.com>
2025-11-30 17:13:40 -0600

    set up type for player info

--- src/main/java/edu/grinnell/csc207/textadventure/TextAdventure.java
+++ src/main/java/edu/grinnell/csc207/textadventure/TextAdventure.java
@@ -1,1 +1,24 @@
 package edu.grinnell.csc207.textadventure;
+
+import java.util.ArrayList;
+
+public class TextAdventure {
+
+    public static class PlayerInfo {
+        public Room room;
+        public Condition condition;
+        private Inventory inventory;
+
+        public PlayerInfo(Room room, Condition cond, Inventory inv) {
+            this.room = room;
+            this.condition = cond;
+            this.inventory = inv;
+        }
+    }
+
+    ArrayList<String> history = new ArrayList<>();
+
+    public static void main(String[] args) {
+
+    }
+}
