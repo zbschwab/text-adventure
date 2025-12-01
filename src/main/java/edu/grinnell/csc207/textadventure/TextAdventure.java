83b0e58017025a115e4006bd973031a651620c43
Z Schwab <zbajraktarischwab@gmail.com>
2025-11-30 19:36:37 -0600

    set up basic game loop with intro and help menu

--- src/main/java/edu/grinnell/csc207/textadventure/TextAdventure.java
+++ src/main/java/edu/grinnell/csc207/textadventure/TextAdventure.java
@@ -1,6 +1,6 @@
 package edu.grinnell.csc207.textadventure;
 
-import java.util.ArrayList;
+import java.util.Scanner;
 
 public class TextAdventure {
 
@@ -16,9 +16,36 @@ public class TextAdventure {
         }
     }
 
-    ArrayList<String> history = new ArrayList<>();
-
     public static void main(String[] args) {
+        String intro = """
+                \n\n\n
 
+                T H E  R I G O R  O F  A N G E L S
+
+
+                a text adventure. type 'help' for guidance. """;
+        String instructions = """
+
+                HINTS: give simple commands. 
+                perceive your environment using verbs like {LOOK, LISTEN, TOUCH, SMELL, TASTE}.
+                interact with your environment using verbs like {CHECK, HOLD, }.
+                wait in silence by pressing ENTER. 
+                ponder matters by using THINK.
+                end the game with EXIT. """;
+
+        Scanner in = new Scanner(System.in);
+        boolean running = true;
+        System.out.println(intro);
+        while (running) {
+            System.out.print("\n> ");
+            String input = in.nextLine().trim();
+            switch (input.toLowerCase()) {
+                case "help" -> System.out.println(instructions);
+                case "exit" -> running = false;
+
+                default -> System.out.println("\nTry something else.");
+            }
+        }
+        in.close();
     }
 }
