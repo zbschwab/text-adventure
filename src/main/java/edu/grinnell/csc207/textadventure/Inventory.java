f9a069aa78c5d90fbf42fcd5bd53d1c43295aa31
Z Schwab <zbajraktarischwab@gmail.com>
2025-12-03 11:08:27 -0600

    switched inventory from arraylist to hashmap

--- src/main/java/edu/grinnell/csc207/textadventure/Inventory.java
+++ src/main/java/edu/grinnell/csc207/textadventure/Inventory.java
@@ -1,5 +1,14 @@
 package edu.grinnell.csc207.textadventure;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Inventory {
-    
+    public class Item {
+        String discovery;
+        String descrip_std;
+    }
+
+    Map<String, Item> invMap = new HashMap<>();
+
 }
