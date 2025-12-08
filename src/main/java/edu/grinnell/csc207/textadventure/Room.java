1d54c6e86bc14735ab206586c5de3f73eaf752c2
Z Schwab <zbajraktarischwab@gmail.com>
2025-12-07 21:19:07 -0600

    restructured room to be an abstract class

--- src/main/java/edu/grinnell/csc207/textadventure/Room.java
+++ src/main/java/edu/grinnell/csc207/textadventure/Room.java
@@ -1,62 +1,33 @@
 package edu.grinnell.csc207.textadventure;
 
-public interface Room {
+import java.util.HashMap;
+import java.util.Map;
 
-    // @return the room's single entrance
-    public String getEntrance();
+public abstract class Room {
+    Map<String, String> text = new HashMap<>();
 
-    // @return description of room from within it
-    public String getDescripIn();
+    void loadText(String file) {
 
-    // @return the room's single exit
-    public String getExit();
+    } 
 
-    // @return description of room from outside of it
-    public String getDescripOut();
+    public abstract void lookResponse(GameState state);
 
-    public class Crate implements Room {
-        private String entrance = "You're folded up in an awkward position. Half your body's gone numb, your head feels fuzzy, and everything smells like tar.";
-        private String descripIn = "You feel coarse, splintery wood grain beneath your fingers. You're in a sealed box of some sort. Hope you're not claustrophobic.";
-        private String exit = "You push the hastily nailed-on box lid up and away. It crashes to the floor. You realize you were in a wooden shipping crate; when you stand up, it seems even smaller from the outside.";
-        private String descripOut = "A wooden shipping crate, with the top lying a few feet away.";
 
-        public Crate(String entrance, String descripIn, String exit, String descripOut) {
-            this.entrance = entrance;
-            this.descripIn = descripIn;
-            this.exit = exit;
-            this.descripOut = descripOut;
+    public class Crate extends Room {
+        public Crate() { loadText("data/crate-room.txt"); }
+
+        @Override 
+        public void lookResponse(GameState state) {
+            
         }
-
-        @Override
-        public String getEntrance() { return entrance; }
-        @Override
-        public String getDescripIn() { return descripIn; }
-        @Override
-        public String getExit() { return exit; }
-        @Override
-        public String getDescripOut() { return descripOut; }
     }
 
-    public class Basement implements Room {
-        private String entrance = "Concrete floor, walls, ceiling. You think you might be in a basement.";
-        private String descripIn = "A small rectangular window at the top of a wall fills the room with dim light. Tfloor is stained, and there's a collection of odd metal circles in the corner.";
-        private String exit = "A rusted metal panel on the ceiling, just above arms reach.";
-        private String descripOut = "";
+    public class Basement extends Room {
+        public Basement() { loadText("data/basement-room.txt"); }
 
-        public Basement(String entrance, String descripIn, String exit, String descripOut) {
-            this.entrance = entrance;
-            this.descripIn = descripIn;
-            this.exit = exit;
-            this.descripOut = descripOut;
+        @Override 
+        public void lookResponse(GameState state) {
+            
         }
-
-        @Override
-        public String getEntrance() { return entrance; }
-        @Override
-        public String getDescripIn() { return descripIn; }
-        @Override
-        public String getExit() { return exit; }
-        @Override
-        public String getDescripOut() { return descripOut; }
     }
 }
