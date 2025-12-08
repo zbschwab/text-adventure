15be18674c360cc1950a26652474e03fc9b8049b
Z Schwab <zbajraktarischwab@gmail.com>
2025-12-07 21:01:11 -0600

    made class for tracking gamestate globally

--- src/main/java/edu/grinnell/csc207/textadventure/GameState.java
+++ src/main/java/edu/grinnell/csc207/textadventure/GameState.java
@@ -1,4 +1,30 @@
 package edu.grinnell.csc207.textadventure;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class GameState {
-    
+    public static class Condition {
+        public boolean bound;
+        public boolean dizzy;
+        public boolean calm;
+        public boolean frightened;
+
+        public Condition() {
+            bound = true;
+            dizzy = true;
+            calm = false;
+            frightened = false;
+        }
+    }
+
+    public Room currentRoom;
+    public Condition condition = new Condition();
+    public List<String> inventory;
+
+    public GameState(Room room, Condition cond) {
+        this.currentRoom = room;
+        this.condition = cond;
+        this.inventory = new ArrayList<>();
+    }
 }
