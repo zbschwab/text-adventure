f38d80b40707ff9e6d3e5d065bfc44f54cf20c30
Z Schwab <zbajraktarischwab@gmail.com>
2025-12-08 20:25:39 -0600

    rewrote game loop handling to work with new Room method, cleaned up logic

--- src/main/java/edu/grinnell/csc207/textadventure/TextAdventure.java
+++ src/main/java/edu/grinnell/csc207/textadventure/TextAdventure.java
@@ -5,34 +5,7 @@ import java.util.Scanner;
 import edu.grinnell.csc207.textadventure.Parser.Action;
 
 public class TextAdventure {
-    public static class Condition {
-        public boolean bound;
-        public boolean dizzy;
-        public boolean calm;
-        public boolean frightened;
-
-        public Condition() {
-            bound = true;
-            dizzy = true;
-            calm = false;
-            frightened = false;
-        }
-    }
-
-    public static class PlayerInfo {
-        public Room room;
-        public Condition condition;
-        public Inventory inventory;
-
-        public PlayerInfo(Room room, Condition cond, Inventory inv) {
-            this.room = room;
-            this.condition = cond;
-            this.inventory = inv;
-        }
-    }
-
-    Condition playerCond = new Condition();
-    Condition otherCond = new Condition();
+    
 
     public static void main(String[] args) {
         String intro = """
@@ -44,33 +17,41 @@ public class TextAdventure {
                 a text adventure. type 'help' for guidance. """;
         String instructions = """
 
-                HINTS: give simple sentence commands. 
+                HINTS: give simple sentence commands.
                 starting with a verb and ending with a subject works best; ex. "Look at the water"
                 perceive your environment using verbs like {LOOK, LISTEN, TOUCH, SMELL, TASTE}.
                 interact with your environment using verbs like {CHECK, HOLD, SING, HIT}.
-                end the game with EXIT. """;
+                end the game with Q. """;
 
         Scanner in = new Scanner(System.in);
-        boolean running = true;
+        GameState state = new GameState(new Crate());
+
         System.out.println(intro);
+
+        boolean running = true;
         while (running) {
             System.out.print("\n> ");
-            String input = in.nextLine().trim();
-            switch (input.toLowerCase()) {
-                case "help" -> System.out.println(instructions);
-                case "exit" -> running = false;
-
-                default -> {
-                    Action act = null;
-                    while (act == null) {
-                        act = Parser.parse(input);
-                        if (act == null) {
-                            System.out.print("\n> ");
-                            input = in.nextLine().trim();
-                        }
-                    }
-                }
+            String input = in.nextLine().trim().toLowerCase();
+            
+            if (input.equals("help")) {
+                System.out.println(instructions);
+                continue;
             }
+
+            if (input.equals("q")) {
+                running = false;
+                continue;
+            }
+
+            Action act = Parser.parse(input);
+            while (act == null) {
+                System.out.print("\n> ");
+                input = in.nextLine().trim();
+                act = Parser.parse(input);
+            }
+
+            Room room = state.currentRoom;
+            room.route(act, state);
         }
         in.close();
     }
