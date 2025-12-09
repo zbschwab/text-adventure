87a3e3c39742fbce0ca9af54e8f3f42ab003fae6
Z Schwab <zbajraktarischwab@gmail.com>
2025-12-08 20:24:26 -0600

    added method to build key from action object for easier lookups

--- src/main/java/edu/grinnell/csc207/textadventure/Room.java
+++ src/main/java/edu/grinnell/csc207/textadventure/Room.java
@@ -1,33 +1,42 @@
 package edu.grinnell.csc207.textadventure;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Scanner;
+
+import edu.grinnell.csc207.textadventure.Parser.Action;
 
 public abstract class Room {
     Map<String, String> text = new HashMap<>();
 
-    void loadText(String file) {
+    final void loadText(String filename) {
+        try (Scanner s = new Scanner(new File(filename));) {
+            while (s.hasNextLine()) {
+                String keyLine = s.nextLine().trim();
+                if (!keyLine.endsWith(":")) { continue; }
+                String key = keyLine.substring(0, keyLine.length() - 1);
 
+                if (s.hasNextLine()) {
+                    String value = s.nextLine().trim();
+                    text.put(key, value);
+                }
+            }
+        } catch (IOException e) {
+            throw new RuntimeException("Error loading room from " + filename, e);
+        }
     } 
 
-    public abstract void lookResponse(GameState state);
+    String keyBuilder(Action act) {
+        String verb = act.verb.toUpperCase();
+        String subject = act.subject.toUpperCase();
+        
+        String key = verb + "_" + subject;
 
-
-    public class Crate extends Room {
-        public Crate() { loadText("data/crate-room.txt"); }
-
-        @Override 
-        public void lookResponse(GameState state) {
-            
-        }
+        return key;
     }
 
-    public class Basement extends Room {
-        public Basement() { loadText("data/basement-room.txt"); }
+    public abstract void route(Action act, GameState state);
 
-        @Override 
-        public void lookResponse(GameState state) {
-            
-        }
-    }
 }
