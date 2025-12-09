19cbaf3f29854f5c30fafc5611f88110529bd111
Z Schwab <zbajraktarischwab@gmail.com>
2025-12-08 20:24:58 -0600

    added keyBuilder from superclass

--- src/main/java/edu/grinnell/csc207/textadventure/Basement.java
+++ src/main/java/edu/grinnell/csc207/textadventure/Basement.java
@@ -1,12 +1,14 @@
 package edu.grinnell.csc207.textadventure;
 
+import edu.grinnell.csc207.textadventure.Parser.Action;
+
 public class Basement extends Room {
     public Basement() {
         loadText("data/basement-room.txt");
     }
 
     @Override
-    public void lookResponse(GameState state) {
-
+    public void route(Action act, GameState state) {
+        System.out.println(text.get(keyBuilder(act)));
     }
 }
--- src/main/java/edu/grinnell/csc207/textadventure/Crate.java
+++ src/main/java/edu/grinnell/csc207/textadventure/Crate.java
@@ -1,12 +1,14 @@
 package edu.grinnell.csc207.textadventure;
 
+import edu.grinnell.csc207.textadventure.Parser.Action;
+
 public class Crate extends Room {
     public Crate() {
         loadText("data/crate-room.txt");
     }
 
     @Override
-    public void lookResponse(GameState state) {
-
+    public void route(Action act, GameState state) {
+        System.out.println(text.get(keyBuilder(act)));
     }
 }
