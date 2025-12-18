88574df82e6860e2066708e4fe0934a348a27507
Z Schwab <zbajraktarischwab@gmail.com>
2025-12-17 18:57:34 -0600

    added conditional logic to modify responses based on gamestate

--- src/main/java/edu/grinnell/csc207/textadventure/Crate.java
+++ src/main/java/edu/grinnell/csc207/textadventure/Crate.java
@@ -9,6 +9,44 @@ public class Crate extends Room {
 
     @Override
     public void route(Action act, GameState state) {
-        System.out.println(text.get(keyBuilder(act)));
+        System.out.println(act.verb + ", " + act.subject);
+
+        // if (act.verb.equals("check") && act.subject.equals("pocket")) {
+
+        // }
+
+        switch (act.subject) {
+            case "exit", "out" -> {
+                if (!state.cond.bound) {
+                    state.changeRoom(new Basement());
+                    act = new Action("try", "exit");
+                } else {
+                    act = new Action("look", "bound");
+                }
+            }
+        }
+
+        switch (act.verb) {
+            case "look" -> {
+                if (state.cond.dizzy) {
+                    act.subject = "dizzy";
+                    state.cond.dizzy = false;
+                    break;
+                }
+                if (!state.cond.acclimated) {
+                    act.subject = "first";
+                    state.cond.acclimated = true;
+                    break;
+                }
+                // act.subject = "around";
+            }
+        }
+
+        String response = text.get(keyBuilder(act));
+        if (response != null) {
+            System.out.println(response);
+        } else {
+            System.out.println("Try something else.");
+        }
     }
 }
