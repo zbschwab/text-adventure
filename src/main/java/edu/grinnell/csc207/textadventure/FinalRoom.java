784f7771f1b388102f02101337931ceea0d378c5
Z Schwab <zbajraktarischwab@gmail.com>
2025-12-19 14:44:16 -0600

    moved endscene logic into the proper file

--- src/main/java/edu/grinnell/csc207/textadventure/FinalRoom.java
+++ src/main/java/edu/grinnell/csc207/textadventure/FinalRoom.java
@@ -8,12 +8,34 @@ public class FinalRoom extends Room {
     }
 
     @Override
+    public Action scriptedAction(GameState state) {
+        return switch (state.turnCount) {
+            case 0 -> new Action("change", "one");
+            case 1 -> new Action("change", "two");
+            case 2 -> new Action("change", "three");
+            // allow user reaction for turn 3
+            case 4 -> new Action("change", "four");
+            // allow user reaction for turn 5
+            case 6 -> new Action("hatch", "opens");
+            case 7 -> new Action("climb", "out");
+            case 8 -> {
+                if (state.cond.angelCalm) {
+                    yield new Action("end", "calm");
+                } else {
+                    yield new Action("end", "default");
+                }
+            }
+            default -> null;
+        };
+    }
+
+    @Override
     public Action route(Action act, GameState state) {
         if (state.turnCount == 3) {
             if (state.cond.angelCalm) {
                 return new Action("frightened", "angel");
             } else if (state.cond.angelFrightened) {
-                return new Action("calm","angel");
+                return new Action("calm", "angel");
             } else {
                 return new Action("neutral", "angel");
             }
